8cc9dd246dff519e48ea5880acea85f808bc5ea4
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-11-21 09:40:03 +0100

    Created FTPStream class
    Simple mockup class that downloads a file every minute from the FTP server.

--- ftp.py
+++ ftp.py
@@ -1,23 +1,50 @@
 import ftplib
 import gzip
+from threading import Thread
+import time
 
 '''''
-# FTP code snippets
+Simple Thread that downloads a file from the FTP server every minute.
 '''''
 
-# Connect to server and print the available files.
-ftp = ftplib.FTP('opendata.ndw.nu')
-ftp.login()
-print(ftp.dir())
 
-# Download a file from the server
-file = open('trafficspeed.xml.gz', 'wb')
-ftp.retrbinary('RETR %s' % 'trafficspeed.xml.gz', file.write)
-file.close()
+class FTPStream(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.ftp = None
 
-# Extract the file and save as xml
-with gzip.open('trafficspeed.xml.gz', 'rb') as file_in:
-    with open('trafficspeed.xml', 'wb') as file_out:
-        for line in file_in:
-            file_out.write(line)
+        self.__connect()
+
+    def __connect(self):
+        self.ftp = ftplib.FTP('opendata.ndw.nu')
+        self.ftp.login()
+
+    def run(self):
+        while True:
+            # Download a file from the server
+            print('Downloading File...')
+            timestr = time.strftime("%Y%m%d%H%M%S", time.localtime())
+            file = open('trafficspeed' + timestr + '.xml.gz', 'wb')
+            self.ftp.retrbinary('RETR %s' % 'trafficspeed.xml.gz', file.write)
+            file.close()
+
+            # Extract the file and save as xml
+            with gzip.open('trafficspeed' + timestr + '.xml.gz', 'rb') as file_in:
+                with open('trafficspeed' + timestr + '.xml', 'wb') as file_out:
+                    for line in file_in:
+                        file_out.write(line)
+
+            # Close all the files
+            file_in.close()
+            file_out.close()
+
+            # Wait for a minute
+            print('Sleeping for 60 seconds')
+            time.sleep(60)
+
+
+stream = FTPStream()
+stream.start()
+
+
 
