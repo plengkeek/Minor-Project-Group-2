afe028a32d2021c44f86d24b5e3fdad4ce77f832
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-11-23 14:32:18 +0100

    Automatic STACK Upload

--- ftp.py
+++ ftp.py
@@ -1,7 +1,8 @@
 import ftplib
-import gzip
 from threading import Thread
-import time
+import time, os
+import easywebdav as wd
+
 
 '''''
 Simple Thread that downloads a file from the FTP server every minute.
@@ -12,31 +13,32 @@ class FTPStream(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.ftp = None
+        self.stack = None
 
-        self.__connect()
+        self.__connect_ftp()
+        self.__connect_stack()
 
-    def __connect(self):
+    def __connect_ftp(self):
         self.ftp = ftplib.FTP('opendata.ndw.nu')
         self.ftp.login()
+
+    def __connect_stack(self):
+        self.stack = wd.connect(host="plengkeek.stackstorage.com", protocol="https", verify_ssl=True,
+                                username='projectgroup', password='wearethebest')
 
     def run(self):
         while True:
             # Download a file from the server
             print('Downloading File...')
             timestr = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            file = open('trafficspeed' + timestr + '.xml.gz', 'wb')
+            file_name = 'trafficspeed' + timestr + '.xml.gz'
+            file = open(file_name, 'wb')
             self.ftp.retrbinary('RETR %s' % 'trafficspeed.xml.gz', file.write)
             file.close()
 
-            # Extract the file and save as xml
-            with gzip.open('trafficspeed' + timestr + '.xml.gz', 'rb') as file_in:
-                with open('trafficspeed' + timestr + '.xml', 'wb') as file_out:
-                    for line in file_in:
-                        file_out.write(line)
-
-            # Close all the files
-            file_in.close()
-            file_out.close()
+            print('Uploading to STACK')
+            self.stack.upload(file_name, "/remote.php/webdav/livedata/" + file_name)
+            os.remove(file_name)
 
             # Wait for a minute
             print('Sleeping for 60 seconds')
