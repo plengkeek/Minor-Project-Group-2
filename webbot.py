fce1dc15b5df0a5e2af7f212da75977caa04aecc
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-11-28 21:38:50 +0100

    Wrapped in class
    TODO:
    - check if captcha is correct
    - create class to upload to STACK
    - fill queue with requests
    - add timeout to waiting for link, readd the request to the queue for next try

--- webbot.py
+++ webbot.py
@@ -5,68 +5,82 @@ import requests
 from twocaptchaapi import TwoCaptchaApi
 import time
 from PIL import Image
+from threading import Thread
+from queue import Queue
 
+q = Queue()
 
-'''''
-You need chrome te be installed on your computer!
-'''''
+class NDWWebBot(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.browser = webdriver.Chrome()
 
-browser = webdriver.Chrome()
-browser.get(('http://83.247.110.3/OpenDataHistorie'))
+    def __open_browser(self):
+        self.browser.get(('http://83.247.110.3/OpenDataHistorie'))
 
-datatype = browser.find_element_by_id("productId")
-start_date = browser.find_element_by_id("fromDate")
-end_date = browser.find_element_by_id("untilDate")
-next_button = browser.find_element_by_id("btnSubmit")
+        self.datatype = self.browser.find_element_by_id("productId")
+        self.start_date = self.browser.find_element_by_id("fromDate")
+        self.end_date = self.browser.find_element_by_id("untilDate")
+        self.next_button = self.browser.find_element_by_id("btnSubmit")
 
-#datatype.send_keys("Intensiteiten en snelheden")
-datatype.send_keys("Configuratie")
-start_date.send_keys("1-11-2017")
-end_date.send_keys("7-11-2017")
+    def __fill_form(self, data_type, start_date, end_data):
+        self.datatype.send_keys(data_type)
+        self.start_date.send_keys(start_date)
+        self.end_date.send_keys(end_data)
 
-# Save the captcha to file
-img = browser.find_element_by_id('CaptchaImage')
-location = img.location
-size = img.size
-browser.save_screenshot('screenshot.png')
-im = Image.open('screenshot.png')
+    def __solve_captcha(self):
+        # Save the captcha to file
+        img = self.browser.find_element_by_id('CaptchaImage')
+        location = img.location
+        size = img.size
+        self.browser.save_screenshot('screenshot.png')
+        im = Image.open('screenshot.png')
 
-src = img.get_attribute('src')
-request.urlretrieve(src, 'captcha.png')
+        src = img.get_attribute('src')
+        request.urlretrieve(src, 'captcha.png')
 
-left = location['x']
-top = location['y']
-right = location['x'] + size['width']
-bottom = location['y'] + size['height']
+        left = location['x']
+        top = location['y']
+        right = location['x'] + size['width']
+        bottom = location['y'] + size['height']
 
-im = im.crop((left, top, right, bottom))
-im.save('captcha.png')
+        im = im.crop((left, top, right, bottom))
+        im.save('captcha.png')
 
-api = TwoCaptchaApi('b51dc904b4f0afc3693977440d8e2e02')
-with open('captcha.png', 'rb') as captcha_file:
-    captcha = api.solve(captcha_file)
-answer = captcha.await_result()
-answer_field = browser.find_element_by_id('CaptchaInputText')
-answer_field.send_keys(answer)
+        api = TwoCaptchaApi('b51dc904b4f0afc3693977440d8e2e02')
+        with open('captcha.png', 'rb') as captcha_file:
+            captcha = api.solve(captcha_file)
+        answer = captcha.await_result()
+        return answer
 
-next_button.click()
-link = browser.find_element_by_id("link").text
+    def run(self):
+        while True:
+            self.__open_browser()
+            data_type, start_date, end_data = q.get()
+            print(data_type, start_date, end_data)
+            self.__fill_form(data_type, start_date, end_data)
+            print('Solving Captcha...')
+            result = self.__solve_captcha()
+            answer_field = self.browser.find_element_by_id('CaptchaInputText')
+            answer_field.send_keys(result)
 
-connected = False
-while not connected:
-    try:
-        # Download the file
-        request.urlretrieve(link, 'file.zip')
-    except error.URLError:
-        print('Link still unavailable')
-        time.sleep(30)
-        continue
+            self.next_button.click()
+            link = self.browser.find_element_by_id("link").text
 
-    connected = True
+            connected = False
+            while not connected:
+                try:
+                    # Download the file
+                    request.urlretrieve(link, 'file.zip')
+                except error.URLError:
+                    print('Link still unavailable')
+                    time.sleep(30)
+                    continue
 
-print('Downloading the file now! :D')
+                connected = True
+                print('Downloading...')
 
-
-
-
-
+thread1 = NDWWebBot()
+q.put(('Configuratie', "1-11-2017", "7-11-2017"))
+q.put(('Configuratie', "1-11-2017", "7-11-2017"))
+thread1.start()
