eb5f5b80cb0a2e2bb61401850791c3794d721e38
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-11-29 14:24:02 +0100

    Managed Queueus

--- webbot.py
+++ webbot.py
@@ -1,14 +1,48 @@
 from selenium import webdriver
 from urllib import request, error
 import time
+from datetime import datetime, timedelta
 import requests
 from twocaptchaapi import TwoCaptchaApi
 import time
 from PIL import Image
 from threading import Thread
 from queue import Queue
+import easywebdav as wd
 
-q = Queue()
+download_q = Queue()
+upload_q = Queue()
+
+start_date = "10-1-2017"
+end_date = "29-11-2017"
+
+start = datetime.strptime(start_date, "%d-%m-%Y")
+stop = datetime.strptime(end_date, "%d-%m-%Y")
+
+while start < stop:
+    download_q.put(('Configuratie', start.strftime("%d-%m-%Y"), start.strftime("%d-%m-%Y")))
+    start = start + timedelta(days=1)
+
+
+class STACKUploader(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.stack = None
+
+    def __connect(self):
+        self.stack = wd.connect(host="plengkeek.stackstorage.com", protocol="https", verify_ssl=True,
+                                username='projectgroup', password='wearethebest')
+
+    def upload(self, folder, file):
+        self.stack.upload(file, "/remote.php/webdav/" + folder + '/' + file)
+
+    def run(self):
+        while True:
+            if not upload_q.empty():
+                file = upload_q.get()
+                self.upload("historicaldata", file)
+            time.sleep(30)
+
 
 class NDWWebBot(Thread):
     def __init__(self):
@@ -17,6 +51,7 @@ class NDWWebBot(Thread):
 
     def __open_browser(self):
         self.browser.get(('http://83.247.110.3/OpenDataHistorie'))
+        self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         self.datatype = self.browser.find_element_by_id("productId")
         self.start_date = self.browser.find_element_by_id("fromDate")
@@ -31,8 +66,8 @@ class NDWWebBot(Thread):
     def __solve_captcha(self):
         # Save the captcha to file
         img = self.browser.find_element_by_id('CaptchaImage')
-        location = img.location
-        size = img.size
+
+        location, size = img.location_once_scrolled_into_view, img.size
         self.browser.save_screenshot('screenshot.png')
         im = Image.open('screenshot.png')
 
@@ -43,6 +78,8 @@ class NDWWebBot(Thread):
         top = location['y']
         right = location['x'] + size['width']
         bottom = location['y'] + size['height']
+
+        print(location['x'], location['y'])
 
         im = im.crop((left, top, right, bottom))
         im.save('captcha.png')
@@ -56,13 +93,20 @@ class NDWWebBot(Thread):
     def run(self):
         while True:
             self.__open_browser()
-            data_type, start_date, end_data = q.get()
+            data_type, start_date, end_data = download_q.get()
             print(data_type, start_date, end_data)
             self.__fill_form(data_type, start_date, end_data)
             print('Solving Captcha...')
+
+
             result = self.__solve_captcha()
+            while len(result) != 2:
+                print(result)
+                result = self.__solve_captcha()
+
             answer_field = self.browser.find_element_by_id('CaptchaInputText')
             answer_field.send_keys(result)
+
 
             self.next_button.click()
             link = self.browser.find_element_by_id("link").text
@@ -71,7 +115,7 @@ class NDWWebBot(Thread):
             while not connected:
                 try:
                     # Download the file
-                    request.urlretrieve(link, 'file.zip')
+                    request.urlretrieve(link, start_date + '.zip')
                 except error.URLError:
                     print('Link still unavailable')
                     time.sleep(30)
@@ -79,8 +123,11 @@ class NDWWebBot(Thread):
 
                 connected = True
                 print('Downloading...')
+                upload_q.put((start_date + '.zip'))
 
-thread1 = NDWWebBot()
-q.put(('Configuratie', "1-11-2017", "7-11-2017"))
-q.put(('Configuratie', "1-11-2017", "7-11-2017"))
-thread1.start()
+
+bot1 = NDWWebBot()
+uploader = STACKUploader()
+
+bot1.start()
+uploader.start()
