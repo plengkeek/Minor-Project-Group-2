0ed524b34be3e9e2d2dac2ec0fac1482a3c1859b
Unknown <g.dacome@student.tudelft.nl>
2017-12-07 16:29:47 +0100

    Last version pa Jose

--- src/Historical/historical_converter.py
+++ src/Historical/historical_converter.py
@@ -10,18 +10,28 @@ class HistoricalConverter(CSVConverter):
 
         for i in range(len(situation_root)):
             situation_xpath = partial(self._xpath, situation_root[i])
+            index_list = []
 
-            measurement_tech = situation_xpath(".//d:computationMethod")[0].text
-
+            id = situation_root[i].attrib["id"]
             latitude_txt = situation_xpath(".//d:latitude")[0].text
             longitude_txt = situation_xpath(".//d:longitude")[0].text
-            name = situation_xpath(".//d:measurementSiteName")[0].text
-            lanes = situation_xpath(".//d:measurementSiteNumberOfLanes")[0].text
+            for index in situation_xpath(".//d:measurementSpecificCharacteristics"):
+                index_data = [x.text for x in index]
 
-            self._buffer_to_write.append(str(name) + ";")
-            self._buffer_to_write.append(str(latitude_txt) + ";")
-            self._buffer_to_write.append(str(longitude_txt) + ";")
-            self._buffer_to_write.append(str(lanes)+";")
+
+
+            try:
+                lanes = situation_xpath(".//d:measurementSiteNumberOfLanes")[0].text
+            except IndexError:
+                lanes = "INVALID NUMBER OF LANES"
+
+            string_to_append = str(id) + "," + str(latitude_txt) + "," + str(longitude_txt) + "," + str(lanes)+ ";"
+
+            self._buffer_to_write.append(string_to_append)
+            # self._buffer_to_write.append(str(lanes)+";")
+
+            # for index in indices_list:
+            #     self._buffer_to_write.append(index.text)
 
 
 
