ee122435f6b1c8c205a714d2f507b6fe7debaf07
Jose <J.I.deAlvearCardenas@student.tudelft.nl>
2017-12-09 20:29:56 +0100

    Updated XML to TXT file with all exceptions.

--- src/Historical/historical_converter.py
+++ src/Historical/historical_converter.py
@@ -9,7 +9,7 @@ class HistoricalConverter(CSVConverter):
 
     def _process(self):
         situation_root = self._xpath(self._xml_tree, ".//d:measurementSiteRecord")
-
+        printable = set(string.printable)
         for i in range(len(situation_root)):
             situation_xpath = partial(self._xpath, situation_root[i])
             index_list = []
@@ -22,7 +22,8 @@ class HistoricalConverter(CSVConverter):
                 latitude_txt = situation_xpath("./d:measurementSiteLocation/d:locationForDisplay/d:latitude")[0].text
             except IndexError:
                 try:
-                    latitude_txt = situation_xpath("./d:measurementSiteLocation/d:locationContainedInItinerary/d:location/d:locationForDisplay/d:latitude")[0].text
+                    latitude_txt = situation_xpath("./d:measurementSiteLocation/d:locationContainedInItinerary/d:location/d:locationForDisplay/d:latitude")[
+                        0].text
                 except IndexError:
                     latitude_txt = "INVALID LATITUDE"
             try:
@@ -36,6 +37,10 @@ class HistoricalConverter(CSVConverter):
                     longitude_txt = "INVALID LONGITUDE"
             try:
                 location = situation_xpath("./d:measurementSiteName/d:values/d:value")[0].text
+                # print str(id) + "--" + location[-2:] + "--" + str(ord(location[-1]))
+                if location[-1] == chr(10):
+                    location = location[:-1]
+                    # print location
             except IndexError:
                 location = "INVALID LOCATION"
             try:
@@ -45,6 +50,16 @@ class HistoricalConverter(CSVConverter):
 
             # Information by index
             root_indices = situation_xpath("./d:measurementSpecificCharacteristics")
+            # if id == "PNH03_N201L_73.52-71.35":
+            #     current_index = partial(self._xpath, root_indices[0])
+            #     print str(id) + "--" + str(root_indices)
+            #     print str(current_index("./d:measurementSpecificCharacteristics/d:specificVehicleCharacteristics/d:vehicleType")[0].text)
+            #     print str(root_indices[0].attrib["index"])
+            #     try:
+            #         lane = current_index("./d:measurementSpecificCharacteristics/d:specificLane")[0].text
+            #     except IndexError:
+            #         lane = "INVALID LANE"
+            #     print str(lane)
             all_indices = ""
             for ix in range(len(root_indices)):
                 current_index = partial(self._xpath,root_indices[ix])
@@ -62,17 +77,22 @@ class HistoricalConverter(CSVConverter):
 
                     all_indices += str(index) + "," + str(lane) + "," + str(type_measurement) + ";"
                 except IndexError:
-                    pass
+                    index = root_indices[ix].attrib["index"]
+                    all_indices += str(index) + "," + str(-1) + "," + str(-1) + ";"
+
+            remaining_indices = ""
+            for i in range(len(root_indices)+1,73):
+                remaining_indices += str(i) + "," + str(-1) + "," + str(-1) + ";"
 
             try:
-                string_to_append = str(id) + "," + str(location) + "," + str(latitude_txt) + "," + str(longitude_txt) + "," + str(lanes)+ ";" + all_indices
+                filtered_location = filter(lambda x: x != ",", location)
+                string_to_append = str(id) + "," + str(filtered_location) + "," + str(latitude_txt) + "," + str(longitude_txt) + "," + str(lanes)+ ";" + all_indices + remaining_indices
                 print string_to_append
                 self._buffer_to_write.append(string_to_append)
             except UnicodeEncodeError:
-                printable = set(string.printable)
-                filtered_location = filter(lambda x: x in printable,location)
+                filtered_location = filter(lambda x: x in printable and x != ",",location)
                 string_to_append = str(id) + "," + str(filtered_location) + "," + str(latitude_txt) + "," + str(
-                    longitude_txt) + "," + str(lanes) + ";"
+                    longitude_txt) + "," + str(lanes) + ";" + all_indices + remaining_indices
                 print string_to_append
                 self._buffer_to_write.append(string_to_append)
 
